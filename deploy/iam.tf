08f38e1f2f6af6e78916e9b9215843e054d4a598
Shira Frumkin <sfrumkin11@gmail.com>
2023-05-28 08:22:33 +0300

    changed tagging and workspace support

--- deploy/iam.tf
+++ deploy/iam.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "lambda_assume_role_policy"{
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "lambda_assume_role_policy"{
   }
 }
 
-resource "aws_iam_role" "lambda_role" {  
-  name = "lambda-lambdaRole-waf"  
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-lambdaRole-waf"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
