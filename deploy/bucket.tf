d7e81f6cd30933117fb3644e3974ba92c2067e1d
sfrumkin <sfrumkin11@gmail.com>
2023-06-06 08:42:14 +0300

    make bucket private

--- deploy/bucket.tf
+++ deploy/bucket.tf
@@ -28,10 +28,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "wordcount-encrypt
 resource "aws_s3_bucket_public_access_block" "wordcount_public_block" {
   bucket = aws_s3_bucket.wordcount-bucket.bucket
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "allow_public_access" {
@@ -41,12 +41,11 @@ resource "aws_s3_bucket_policy" "allow_public_access" {
     Id      = "MYBUCKETPOLICY"
     Statement = [
       {
-        Sid       = "PublicReadListGetObject"
+        Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
         Action = [
-          "s3:GetObject",
-          "s3:ListBucket",
+          "s3:GetObject"
         ]
         Resource = [
           "${aws_s3_bucket.wordcount-bucket.arn}",
